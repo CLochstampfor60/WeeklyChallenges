6cdb2ee8a6ad42c0fec12b440f432711dad4c769
CLochstampfor60 <cflochjr@gmail.com>
2023-10-04 16:03:26 -0400

    C3 25% complete

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ChallengesWithTestsMark8
 {
@@ -8,21 +11,52 @@ namespace ChallengesWithTestsMark8
         public bool ArrayContainsAFalse(bool[] vals)
         {
             throw new NotImplementedException();
+
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || !numbers.Any()) return false;
+
+            int count = 0;
+
+/*            for (int i = 0; i < numbers.Count(); i++)
+                count += i;
+
+            if (count % 2 != 0) return true;
+            return false;*/
+
+            foreach (var n in numbers)
+            {
+                count += n;
+            }
+
+            if (count % 2 == 0) return false;
+            else return true;
+
+
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
+            /*     for (int i = 0; i < password.Length; i++)
+                 {
+                     foreach(char c in password)
+                     {
+                         if (c == ' ') return false;
+                         if (c % 2 == 0) return true;
+
+                     }
+
+                 }*/
             throw new NotImplementedException();
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            
+            return val.ToCharArray()[0];
+               
         }
 
         public char GetLastLetterOfString(string val)
@@ -42,12 +76,23 @@ namespace ChallengesWithTestsMark8
 
         public int[] GetOddsBelow100()
         {
+            /*  var start = new int[1];*/
+
+            /*  for (var i = 0; i <= 100; i++)
+              {
+                  start[i] = i;
+                  i++;
+                  return start;
+              }*/
             throw new NotImplementedException();
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].ToUpper();
+            }
         }
     }
 }
